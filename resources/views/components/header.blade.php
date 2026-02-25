222107ccaf8c6d88e832bc126c0ecafea8cf864c
pefiye <pefiyeplayz@gmail.com>
2026-02-25 10:42:01 +0700

    update page admin and user homepage

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,10 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homade</title>
+    @vite([
+			'sass/app.scss', 
+			'sass/metronic/style.scss', 
+		])
+</head>
